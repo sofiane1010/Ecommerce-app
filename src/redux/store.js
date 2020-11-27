d351227f17805e56bcd7e998f7c4e3ed9a4b6212
Sofiane <daimellah.sofiane@gmail.com>
2020-11-27 17:27:29 +0100

    fixed a bug when disabling redux devtools in production mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(
-	rootReducer,
-	process.env.NODE_ENV === "development"
-		? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-				window.__REDUX_DEVTOOLS_EXTENSION__()
-		: null
-);
+let store;
+if (process.env.NODE_ENV === "development")
+	store = createStore(
+		rootReducer,
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	);
+else store = createStore(rootReducer);
 
 export default store;
